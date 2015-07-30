f131c3d050218d99a1989365d4b631c5daa69df0
josmanlerock <josman_mtz@hotmail.com>
2015-07-30 07:46:52 -0500

    Update yaourt.sh
    actualizado 31-julio-2015

--- yaourt.sh
+++ yaourt.sh
@@ -6,14 +6,14 @@ curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
 tar zxvf package-query.tar.gz
 cd package-query
 makepkg PKGBUILD
-sudo pacman -U package-query-1.5-2-x86_64.pkg.tar.xz
+sudo pacman -U package-query-*.pkg.tar.xz
 cd ..
 curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
 tar zxvf yaourt.tar.gz
 cd yaourt
 yaourt.tar.gz
 makepkg PKGBUILD
-sudo pacman -U yaourt-1.5-1-any.pkg.tar.xz
+sudo pacman -U yaourt-*.pkg.tar.xz
 cd ..
 rm -r package-query yaourt
 rm package-query.tar.gz yaourt.tar.gz
